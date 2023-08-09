2b298008294a19c21754b81c3610f82886ae9006
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-09 08:42:12 +0000

    frontend :: installed tremor for dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, Flex, Metric, ProgressBar, Text } from "@tremor/react";
+// import "@tremor/react/dist/esm/tremor.css";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import "./App.css";
@@ -34,6 +36,15 @@ function App() {
   }
   return (
     <div>
+      <Card>
+        <Text>Sales</Text>
+        <Metric>$ 60,465</Metric>
+        <Flex>
+          <Text>52% of annual target</Text>
+          <Text>$ 254,000</Text>
+        </Flex>
+        <ProgressBar value={42} />
+      </Card>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
+  ],
   theme: {
     container: {
       center: true,
@@ -73,4 +74,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
