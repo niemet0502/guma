1cd7ed89c706223bf74cb65661d6f26114556320
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-11 16:09:29 +0000

    frontend :: customized the scrollbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,67 +1,9 @@
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
 // import "@tremor/react/dist/esm/tremor.css";
-import { ModeToggle } from "@/components/mode-toggle";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
 import "./App.css";
+import { Layout } from "./Layout/Layout";
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
 function App() {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
-
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
-  return (
-    <div>
-      <ModeToggle />
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="submit-button">
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </div>
-  );
+  return <Layout />;
 }
 
 export default App;
