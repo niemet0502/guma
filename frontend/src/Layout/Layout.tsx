acf2c72900f03df17bdb5d78277b4a7fd9d9eb14
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-11 16:08:43 +0000

    frontend :: created the main layout

---
+++ frontend/src/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "./components/Sidebar";
+import { Topbar } from "./components/Topbar";
+
+export const Layout: React.FC = () => {
+  return (
+    <div className="w-full flex h-screen fixed">
+      <Sidebar />
+      <div className="col-span-3 flex flex-1 flex-col lg:border-l">
+        <Topbar />
+        <div className="overflow-auto">
+          <div className="h-[1200px]">Outlet</div>
+        </div>
+      </div>
+    </div>
+  );
+};
