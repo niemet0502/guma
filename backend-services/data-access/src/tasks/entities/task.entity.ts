c5cd2779f14d5232b9edd96fc1a1e7f0be91e275
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-19 14:01:30 +0000

    backend :: create the task entity

--- backend-services/data-access/src/tasks/entities/task.entity.ts
+++ backend-services/data-access/src/tasks/entities/task.entity.ts
@@ -1,1 +1,36 @@
-export class Task {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskType } from '../tasks.enum';
+
+@Entity()
+export class Task {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text', { nullable: false })
+  slug: string;
+
+  @Column('text', { nullable: false })
+  name: string;
+
+  @Column({
+    type: 'enum',
+    enum: TaskType,
+    default: TaskType.ISSUE,
+  })
+  type: TaskType.ISSUE;
+
+  @Column('number', { nullable: true })
+  priority: number;
+
+  @Column('number', { nullable: false })
+  created_by: number;
+
+  @Column('number', { nullable: true })
+  parent_task_id: number;
+
+  @Column('number', { nullable: false })
+  sprint_id: number;
+
+  @Column('number', { nullable: false })
+  status_id: number;
+}
