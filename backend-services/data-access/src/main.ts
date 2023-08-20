2b22421a88eb28bea1ea0fa99619708e2e1724dd
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-20 23:26:07 +0000

    backend :: [DataAccessService] setup input validation for the orga resource

--- backend-services/data-access/src/main.ts
+++ backend-services/data-access/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -10,6 +11,8 @@ async function bootstrap() {
     .setDescription('The Neka documentation')
     .setVersion('0.1')
     .build();
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- backend-services/data-access/src/organizations/organizations.controller.ts
+++ backend-services/data-access/src/organizations/organizations.controller.ts
@@ -7,30 +7,37 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
+import { Organization } from './entities/organization.entity';
 import { OrganizationsService } from './organizations.service';
 
 @Controller('organizations')
+@ApiTags('organizations')
 export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Organization })
   create(@Body() createOrganizationDto: CreateOrganizationDto) {
     return this.organizationsService.create(createOrganizationDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: Organization, isArray: true })
   findAll() {
     return this.organizationsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Organization })
   findOne(@Param('id') id: string) {
     return this.organizationsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: Organization })
   update(
     @Param('id') id: string,
     @Body() updateOrganizationDto: UpdateOrganizationDto,
@@ -39,6 +46,7 @@ export class OrganizationsController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: Organization })
   remove(@Param('id') id: string) {
     return this.organizationsService.remove(+id);
   }
