d9a504646d3e5ca70e14998d2e30b6f80bd3b204
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-23 10:13:47 +0000

    backend :: [DataAccessService] added documentation for the tasks' endpoints

--- backend-services/data-access/src/tasks/tasks.controller.ts
+++ backend-services/data-access/src/tasks/tasks.controller.ts
@@ -8,7 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './entities/task.entity';
@@ -20,27 +20,32 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Task })
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: Task, isArray: true })
   findAll(@Query('team_id') team_id: string): Promise<Task[]> {
     // TODO add search params
     return this.tasksService.findAll(+team_id);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Task })
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: Task })
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: Task })
   remove(@Param('id') id: string) {
     return this.tasksService.remove(+id);
   }
