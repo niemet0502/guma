94b818a8d49e2feedcb3dfc92882935638e5fea2
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-27 14:19:32 +0000

    backend :: [DataAccessService] added the documentation for documents' endpoints

--- backend-services/data-access/src/documents/documents.controller.ts
+++ backend-services/data-access/src/documents/documents.controller.ts
@@ -8,31 +8,38 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { DocumentsService } from './documents.service';
 import { CreateDocumentDto } from './dto/create-document.dto';
 import { UpdateDocumentDto } from './dto/update-document.dto';
+import { Document } from './entities/document.entity';
 
 @Controller('documents')
+@ApiTags('documents')
 export class DocumentsController {
   constructor(private readonly documentsService: DocumentsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: Document })
   create(@Body() createDocumentDto: CreateDocumentDto) {
     // TODO get the user.id from the header
     return this.documentsService.create(createDocumentDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: Document, isArray: true })
   findAll(@Query('team_id') team_id: string) {
     return this.documentsService.findAllByTeam(+team_id);
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: Document })
   findOne(@Param('id') id: string) {
     return this.documentsService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ type: Document })
   update(
     @Param('id') id: string,
     @Body() updateDocumentDto: UpdateDocumentDto,
@@ -41,6 +48,7 @@ export class DocumentsController {
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: Document })
   remove(@Param('id') id: string) {
     return this.documentsService.remove(+id);
   }
