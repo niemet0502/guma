9b67b4c97b27fffd0ef8dd8aa6cbdb022f95ce85
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-08-31 18:25:13 +0000

    backend :: [WikiService] implemented document queries and mutations

---
+++ backend-services/wiki/src/documents/documents.resolver.ts
@@ -0,0 +1,43 @@
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { DocumentsService } from './documents.service';
+import { CreateDocumentInput } from './dto/create-document.input';
+import { UpdateDocumentInput } from './dto/update-document.input';
+import { Document } from './entities/document.entity';
+
+@Resolver(() => Document)
+export class DocumentsResolver {
+  constructor(private readonly documentsService: DocumentsService) {}
+
+  @Mutation(() => Document)
+  createDocument(
+    @Args('createDocumentInput') createDocumentInput: CreateDocumentInput,
+  ) {
+    // TODO get the user.id from the context
+    return this.documentsService.create(createDocumentInput);
+  }
+
+  @Query(() => [Document], { name: 'documents' })
+  findAll(@Args('team_id', { type: () => Int }) team_id: number) {
+    return this.documentsService.findAllByTeam(team_id);
+  }
+
+  @Query(() => Document, { name: 'document' })
+  findOne(@Args('id', { type: () => Int }) id: number) {
+    return this.documentsService.findOne(id);
+  }
+
+  @Mutation(() => Document)
+  updateDocument(
+    @Args('updateDocumentInput') updateDocumentInput: UpdateDocumentInput,
+  ) {
+    return this.documentsService.update(
+      updateDocumentInput.id,
+      updateDocumentInput,
+    );
+  }
+
+  @Mutation(() => Document)
+  removeDocument(@Args('id', { type: () => Int }) id: number) {
+    return this.documentsService.remove(id);
+  }
+}
---
+++ backend-services/wiki/src/documents/dto/create-document.input.ts
@@ -0,0 +1,23 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { DocumentStatus } from '../documents.enum';
+
+@InputType()
+export class CreateDocumentInput {
+  @Field({ nullable: false })
+  name: string;
+
+  @Field({ nullable: true })
+  folder_id: number;
+
+  @Field({ nullable: true })
+  team_id: number;
+
+  @Field()
+  created_by: number;
+
+  @Field({ nullable: true })
+  content: string;
+
+  @Field({ nullable: true })
+  status: DocumentStatus;
+}
