97751d82e60d88e8d436d1a128714b0df6a54e62
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-01 21:43:18 +0000

    backend :: [DataAccessService] implemented the workflow CRUD endpoints

--- backend-services/data-access/src/teams/teams.module.ts
+++ backend-services/data-access/src/teams/teams.module.ts
@@ -5,21 +5,29 @@ import { UsersModule } from 'src/users/users.module';
 import { Member } from './entities/member.entity';
 import { TaskStatus } from './entities/status.entity';
 import { Team } from './entities/team.entity';
+import { Workflow } from './entities/workflow.entity';
 import { MembersController } from './members.controller';
 import { MembersService } from './members.service';
 import { StatusController } from './status.controller';
 import { StatusService } from './status.service';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
+import { WorkflowController } from './workflow.controller';
+import { WorkflowService } from './workflow.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TaskStatus, Team, Member]),
+    TypeOrmModule.forFeature([TaskStatus, Team, Member, Workflow]),
     OrganizationsModule,
     UsersModule,
   ],
-  controllers: [TeamsController, StatusController, MembersController],
-  providers: [TeamsService, StatusService, MembersService],
+  controllers: [
+    TeamsController,
+    StatusController,
+    MembersController,
+    WorkflowController,
+  ],
+  providers: [TeamsService, StatusService, MembersService, WorkflowService],
   exports: [TeamsService, StatusService],
 })
 export class TeamsModule {}
---
+++ backend-services/data-access/src/teams/workflow.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateWorkflowDto } from './dto/create-workflow.dto';
+import { UpdateWorkflowDto } from './dto/update-workflow.dto';
+import { Workflow } from './entities/workflow.entity';
+import { StatusService } from './status.service';
+import { TeamsService } from './teams.service';
+
+@Injectable()
+export class WorkflowService {
+  constructor(
+    @InjectRepository(Workflow)
+    private workflowRepository: Repository<Workflow>,
+    private readonly teamService: TeamsService,
+    private readonly statusService: StatusService,
+  ) {}
+
+  async create(createWorkflowDto: CreateWorkflowDto): Promise<Workflow> {
+    const { team_id, status_id } = createWorkflowDto;
+
+    const team = await this.teamService.findOne(team_id);
+
+    if (!team) {
+      throw new NotFoundException('Team not found');
+    }
+
+    const status = await this.statusService.findOne(team_id);
+
+    if (!status) {
+      throw new NotFoundException('Status not found');
+    }
+
+    return await this.workflowRepository.save(createWorkflowDto);
+  }
+
+  async findAllByTeam(team_id: number): Promise<Workflow[]> {
+    return await this.workflowRepository.find({ where: { team_id } });
+  }
+
+  async update(
+    id: number,
+    updateWorkflowDto: UpdateWorkflowDto,
+  ): Promise<Workflow> {
+    const toUpdate = await this.workflowRepository.findOne({
+      where: { id },
+    });
+
+    const updated = Object.assign(toUpdate, updateWorkflowDto);
+
+    return await this.workflowRepository.save(updated);
+  }
+
+  async remove(id: number): Promise<Workflow> {
+    const workflow = await this.workflowRepository.findOne({ where: { id } });
+
+    if (!workflow) {
+      throw new NotFoundException('TaskStatus not found');
+    }
+
+    return await this.workflowRepository.remove(workflow);
+  }
+}
