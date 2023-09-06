13c23227990dcb73af5c4be213c8ed95478e08cd
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-06 17:02:08 +0000

    gateway :: added the sprint field on the activity Object type

--- backend-services/issues/src/activities/activities.resolver.ts
+++ backend-services/issues/src/activities/activities.resolver.ts
@@ -8,6 +8,7 @@ import {
   Resolver,
 } from '@nestjs/graphql';
 import { User } from '../shared/user.entity';
+import { Sprint } from '../sprints/entities/sprint.entity';
 import { ActivitiesService } from './activities.service';
 import { CreateActivityInput } from './dto/create-activity.input';
 import { Activity } from './entities/activity.entity';
@@ -36,5 +37,11 @@ export class ActivitiesResolver {
   @ResolveField(() => User)
   author(@Parent() activity: Activity): any {
     return { __typename: 'User', id: activity.created_by };
+  }
+
+  @ResolveField((type) => Sprint)
+  async sprint(@Parent() activity: Activity) {
+    const { sprint_id } = activity;
+    return await this.activitiesService.getSprint(sprint_id);
   }
 }
