5af35a32b01bc3f82a73b3deded0305eff54eb9c
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-12 20:50:21 +0000

    frontend :: created a AuthenticationGuard and ProtectedRoute component for route handling

--- frontend/src/router/components/AuthenticationGuard.tsx
+++ frontend/src/router/components/AuthenticationGuard.tsx
@@ -1,30 +1,26 @@
-import * as React from "react";
-import { useAuth } from "../../domains/auth/providers/auth";
+import { useAuth } from "@/domains/auth/providers/auth";
+import type { FC, ReactElement } from "react";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export type AuthenticationGuardProps = {
-  children?: React.ReactElement;
+  children?: ReactElement;
   redirectPath?: string;
+  guardType?: "authenticated" | "unauthenticated";
 };
 
-export const AuthenticationGuard: React.FC<AuthenticationGuardProps> = ({
-  redirectPath = "/",
+export const AuthenticationGuard: FC<AuthenticationGuardProps> = ({
+  redirectPath = "/login",
+  guardType = "authenticated",
   ...props
 }) => {
   const { user } = useAuth();
+  const isAllowed = guardType === "authenticated" ? !!user : !user;
 
   return (
-    <ProtectedRoute redirectPath={redirectPath} isAllowed={!!user} {...props} />
-  );
-};
-
-export const UnAuthenticationGuard: React.FC<AuthenticationGuardProps> = ({
-  redirectPath = "/",
-  ...props
-}) => {
-  const { user } = useAuth();
-
-  return (
-    <ProtectedRoute redirectPath={redirectPath} isAllowed={!user} {...props} />
+    <ProtectedRoute
+      redirectPath={redirectPath}
+      isAllowed={isAllowed}
+      {...props}
+    />
   );
 };
--- frontend/src/router/components/ProtectedRoute.tsx
+++ frontend/src/router/components/ProtectedRoute.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
+import { FC, ReactElement } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-export type ProtectedRouteProps = { children?: React.ReactElement } & {
+export type ProtectedRouteProps = { children?: ReactElement } & {
   isAllowed: boolean;
   redirectPath?: string;
 };
 
-export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({
   isAllowed,
   children,
   redirectPath = "/",
