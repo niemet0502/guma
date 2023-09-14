ba451e012a6c1b17b3c90cb6310576cfccedfe86
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-14 20:51:11 +0000

    frontend :: setup ApolloClient provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,23 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Toaster } from "./components/ui/toaster.tsx";
 import "./index.css";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
 
+const client = new ApolloClient({
+  uri: "http://localhost:5007/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
