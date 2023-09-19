18268b929fe449e6ccf2bd3f964d8b51aaf987fe
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-19 22:23:04 +0000

    backend :: [DataAccessService] implemented login and logout features

--- backend-services/data-access/src/auth/auth.module.ts
+++ backend-services/data-access/src/auth/auth.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserSession } from './entities/session.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([UserSession]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
--- backend-services/data-access/src/auth/auth.service.ts
+++ backend-services/data-access/src/auth/auth.service.ts
@@ -1,4 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as jwt from 'jsonwebtoken';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { UserSession } from './entities/session.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserSession)
+    private userSessionRepository: Repository<UserSession>,
+  ) {}
+
+  async createSession(user: User) {
+    const { id } = user;
+    const session = new UserSession();
+    let today = new Date();
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        lastname: user.lastname,
+        firstname: user.firstname,
+      },
+      'shhhhh',
+    );
+
+    session.user_id = id;
+    session.token = token;
+    session.created_at = new Date().toLocaleString();
+    session.expired_at = new Date(
+      today.setMonth(today.getMonth() + 2),
+    ).toLocaleString();
+
+    return await this.userSessionRepository.save(session);
+  }
+
+  async findOne(token: string) {
+    return await this.userSessionRepository.findOne({
+      where: { token: token },
+    });
+  }
+
+  async remove(session: UserSession) {
+    return await this.userSessionRepository.remove(session);
+  }
+
+  //   async removeAll(user: User) {
+  //     return await this.userSessionRepository.delete({ where: { use_id: user.id} });
+  //   }
+}
