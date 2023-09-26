d4614f16b43bd14c69e27bd6c10254cfe606535b
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-09-26 19:03:11 +0000

    frontend :: added the dialog component

--- frontend/src/domains/teams/components/TeamCard.tsx
+++ frontend/src/domains/teams/components/TeamCard.tsx
@@ -92,55 +92,6 @@ export const TeamCard: React.FC<{ team: TeamApi }> = ({ team }) => {
           </AccordionItem>
         </Accordion>
       </div>
-      {/* <div className="px-4 flex items-center justify-between">
-        <h5 className="my-2  text-base  font-semibold tracking-tight">
-          {team.name}
-        </h5>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <BiDotsHorizontalRounded />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>Team settings</DropdownMenuItem>
-            <DropdownMenuItem>Copy the link</DropdownMenuItem>
-            <DropdownMenuItem disabled={isPublic}>
-              Leave the team
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-
-      <div className="space-y-1 pl-4 pr-3">
-        <NavLink
-          to={`/${orgaId}/team/${team.name}/issues`}
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "default" : "default"
-          }
-        >
-          <GoIssueDraft className="text-base" />
-          Issues
-        </NavLink>
-        <NavLink
-          to={`/${orgaId}/team/${team.name}/documents`}
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "default" : "default"
-          }
-        >
-          <HiOutlineDocumentDuplicate className="text-base" />
-          Documents
-        </NavLink>
-
-        <NavLink
-          to={`/${orgaId}/team/${team.name}/sprints`}
-          className={({ isActive, isPending }) =>
-            isActive ? "active" : isPending ? "default" : "default"
-          }
-        >
-          <AiOutlinePlayCircle />
-          Sprints
-        </NavLink>
-      </div> */}
     </div>
   );
 };
