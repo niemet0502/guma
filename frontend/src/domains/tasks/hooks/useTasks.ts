148e13784680adf79e648ed5b22974f665419a6f
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-10-03 14:13:27 +0000

    frontend :: created a reusable custom hook to fetch tasks list by team

---
+++ frontend/src/domains/tasks/hooks/useTasks.ts
@@ -0,0 +1,62 @@
+import { gql, useQuery } from "@apollo/client";
+import { TaskApi } from "../type";
+
+const GET_TASKS_BY_TEAM = gql`
+  query GetTasksByTeam(
+    # $team_id: Int!
+    $type: Int
+    $status_id: Int
+    $parent_task_id: Int
+    $sprint_id: Int
+  ) {
+    tasks(
+      team_id: 20
+      type: $type
+      status_id: $status_id
+      parent_task_id: $parent_task_id
+      sprint_id: $sprint_id
+    ) {
+      id
+      name
+      identifier
+      created_at
+
+      assignee_to
+      assignee {
+        id
+        username
+      }
+
+      team {
+        id
+        members {
+          id
+          user {
+            id
+            username
+          }
+        }
+      }
+
+      labels {
+        id
+        label {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const useTasks = () => {
+  const {
+    data,
+    loading: isLoading,
+    error,
+  } = useQuery<{
+    tasks: TaskApi[];
+  }>(GET_TASKS_BY_TEAM);
+
+  return { data: data?.tasks, isLoading, error };
+};
