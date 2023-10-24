e8153e90a1e6222726a6c4d3bffd00e448bad3cb
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-10-24 20:34:41 +0000

    frontend :: created sprint badge component

---
+++ frontend/src/domains/sprints/components/SprintBadge.tsx
@@ -0,0 +1,11 @@
+import { Badge } from "@/components/ui/badge";
+import { AiOutlinePlayCircle } from "react-icons/ai";
+
+export const SprintBadge: React.FC<{ name: string }> = ({ name }) => {
+  return (
+    <Badge variant="secondary" className="flex gap-1">
+      <AiOutlinePlayCircle />
+      {name}
+    </Badge>
+  );
+};
