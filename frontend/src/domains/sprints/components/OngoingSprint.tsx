2b7c1c06727b6e9b724dcb8a98b0a557159c3ebb
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-11-22 17:20:30 +0000

    frontend :: displayed tasks by status for the in progress status

--- frontend/src/domains/sprints/components/OngoingSprint.tsx
+++ frontend/src/domains/sprints/components/OngoingSprint.tsx
@@ -1,8 +1,42 @@
+import { TaskCard } from "@/domains/tasks/components/TaskCard";
+import { TaskStatusIcon } from "@/domains/tasks/components/TaskStatusIcon";
 import { useGetStatus } from "@/domains/tasks/hooks/useGetStatus";
 import { SprintApi } from "@/domains/tasks/type";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
 
 export const OngoingSprint: React.FC<{ sprint: SprintApi }> = ({ sprint }) => {
   const { data: status } = useGetStatus(sprint.team_id);
 
-  return <div>{JSON.stringify(status)}</div>;
+  return (
+    <div className="w-full h-full flex flex-col">
+      <div className="mb-3 bg-secondary py-3 px-5 flex items-center gap-1 sticky top-0">
+        <NavLink to={`/ `} className="hover:text-muted-foreground w-auto p-0">
+          Sprints
+        </NavLink>
+        <p className="flex gap-1 items-center">
+          <RiArrowRightSLine className="mt-0.5" />
+          {sprint?.name}
+        </p>
+      </div>
+      <div className="w-full h-full flex overflow-x-auto">
+        {status?.map((statut) => (
+          <div key={statut.id} className="w-[330px] p-2 flex flex-col gap-3">
+            <div className="flex gap-2 items-center">
+              <TaskStatusIcon status={statut.name} />
+              {statut.name}
+            </div>
+
+            <div className="flex flex-col gap-3">
+              {sprint.tasks
+                ?.filter(({ status }) => status.id === statut.id)
+                .map((task) => (
+                  <TaskCard task={task} key={task.id} />
+                ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- frontend/src/domains/sprints/pages/Details.tsx
+++ frontend/src/domains/sprints/pages/Details.tsx
@@ -5,10 +5,17 @@ import { useGetSprint } from "../hooks/useGetSprint";
 export const SprintDetails: React.FC = () => {
   const { sprintId } = useParams<{ sprintId: string }>();
 
-  const { data } = useGetSprint(sprintId as string);
+  const { data: sprint } = useGetSprint(sprintId as string);
 
-  if (data && !data.isCompleted) {
-    return <OngoingSprint sprint={data} />;
+  if (sprint && !sprint.isCompleted) {
+    return <OngoingSprint sprint={sprint} />;
   }
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div className="w-full">
+      <div className="mb-3 bg-secondary py-3 px-5 flex items-center justify-between sticky top-0">
+        <p>Sprints</p>
+        {JSON.stringify(sprint)}
+      </div>
+    </div>
+  );
 };
