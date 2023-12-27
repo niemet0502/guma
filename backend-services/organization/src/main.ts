338fed37913caef5331f3723dcf000576f66bccb
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-12-27 01:10:12 +0000

    backend :: [OrganizationService] removed unused comment

--- backend-services/organization/src/main.ts
+++ backend-services/organization/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // testing comment
   await app.listen(5001);
 }
 bootstrap();
