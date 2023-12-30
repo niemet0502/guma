a4ff1b902006c969f9d76136c30165904dd8b1ee
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-12-30 17:22:56 +0000

    backend :: [IssueService] updated the tracing setup

--- backend-services/issues/src/tracing.ts
+++ backend-services/issues/src/tracing.ts
@@ -1,26 +1,33 @@
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
+import { Resource } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import {
   ConsoleSpanExporter,
   SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import * as process from 'process';
 
 const traceExporter = new ConsoleSpanExporter();
 
-const oltpExporter = new OTLPTraceExporter({
-  url: `http://localhost:55680`,
+const jaegerExporter = new JaegerExporter({
+  endpoint: 'http://localhost:14268/api/traces',
 });
 
 export const otelSDK = new NodeSDK({
-  spanProcessor: new SimpleSpanProcessor(oltpExporter),
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: `issues`,
+  }),
+  spanProcessor: new SimpleSpanProcessor(jaegerExporter),
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
     new GraphQLInstrumentation(),
+    new NestInstrumentation(),
   ],
 });
 
