a64df18a7bc2eda396835b942f3313abd303f5b1
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-12-30 15:59:31 +0000

    gateway :: setup tracing

--- gateway/src/tracing.ts
+++ gateway/src/tracing.ts
@@ -1,12 +1,14 @@
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { Resource } from '@opentelemetry/resources';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import {
+  BatchSpanProcessor,
   ConsoleSpanExporter,
-  SimpleSpanProcessor,
 } from '@opentelemetry/sdk-trace-base';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import * as process from 'process';
@@ -14,18 +16,23 @@ import * as process from 'process';
 const traceExporter = new ConsoleSpanExporter();
 
 const oltpExporter = new OTLPTraceExporter({
-  url: `http://localhost:62691/`,
+  url: `http://localhost:62691/api/traces`,
+});
+
+const jaegerExporter = new JaegerExporter({
+  endpoint: 'http://localhost:14268/api/traces',
 });
 
 export const otelSDK = new NodeSDK({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: `gateway`,
   }),
-  spanProcessor: new SimpleSpanProcessor(oltpExporter),
+  spanProcessor: new BatchSpanProcessor(jaegerExporter),
   instrumentations: [
     new HttpInstrumentation(),
     new ExpressInstrumentation(),
     new GraphQLInstrumentation(),
+    new NestInstrumentation(),
   ],
 });
 
