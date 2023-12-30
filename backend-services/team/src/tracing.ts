66f6d0beec1fd0784e566d555623863a7d0abad7
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2023-12-30 01:33:59 +0000

    backend :: [TeamService] setup tracing

---
+++ backend-services/team/src/tracing.ts
@@ -0,0 +1,31 @@
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { GraphQLInstrumentation } from '@opentelemetry/instrumentation-graphql';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from '@opentelemetry/sdk-trace-base';
+import * as process from 'process';
+
+const traceExporter = new ConsoleSpanExporter();
+
+export const otelSDK = new NodeSDK({
+  spanProcessor: new SimpleSpanProcessor(traceExporter),
+  instrumentations: [
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new GraphQLInstrumentation(),
+  ],
+});
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  otelSDK
+    .shutdown()
+    .then(
+      () => console.log('SDK shut down successfully'),
+      (err) => console.log('Error shutting down SDK', err),
+    )
+    .finally(() => process.exit(0));
+});
