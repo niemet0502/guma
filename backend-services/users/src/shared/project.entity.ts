0f7cad28daaa7c1a1160b57eab1b09e113c5b3ae
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-01-17 14:48:09 +0000

    backend :: [ProjectService] renamed the organization entity

--- backend-services/users/src/shared/project.entity.ts
+++ backend-services/users/src/shared/project.entity.ts
@@ -3,7 +3,7 @@ import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 @ObjectType()
 @Directive('@extends')
 @Directive('@key(fields: "id")')
-export class Organization {
+export class Project {
   @Field((type) => ID)
   @Directive('@external')
   id: number;
