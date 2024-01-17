14a509f63c455e1f513054dd4c03ae0286c0f9a3
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-01-17 12:20:17 +0000

    backend :: [DataAccessService] created a seperate activities module

--- backend-services/data-access/src/app.module.ts
+++ backend-services/data-access/src/app.module.ts
@@ -2,19 +2,32 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { dataSourceOptions } from './database/data-source';
-import { OrganizationsModule } from './organizations/organizations.module';
-import { UsersModule } from './users/users.module';
-import { TeamsModule } from './teams/teams.module';
-import { TasksModule } from './tasks/tasks.module';
-import { DocumentsModule } from './documents/documents.module';
 import { AuthModule } from './auth/auth.module';
+import { dataSourceOptions } from './database/data-source';
+import { DocumentsModule } from './documents/documents.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 import { SprintsModule } from './sprints/sprints.module';
-import { WorkflowModule } from './workflow/workflow.module';
 import { StatusModule } from './status/status.module';
+import { TasksModule } from './tasks/tasks.module';
+import { TeamsModule } from './teams/teams.module';
+import { UsersModule } from './users/users.module';
+import { WorkflowModule } from './workflow/workflow.module';
+import { ActivitiesModule } from './activities/activities.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), OrganizationsModule, UsersModule, TeamsModule, TasksModule, DocumentsModule, AuthModule, SprintsModule, WorkflowModule, StatusModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    OrganizationsModule,
+    UsersModule,
+    TeamsModule,
+    TasksModule,
+    DocumentsModule,
+    AuthModule,
+    SprintsModule,
+    WorkflowModule,
+    StatusModule,
+    ActivitiesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
