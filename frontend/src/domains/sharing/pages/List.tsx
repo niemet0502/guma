0701d694e7f792a1ae9fe3e34295f2b920d33e75
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-01-23 14:54:32 +0000

    frontend :: created a new page for questions lis t

---
+++ frontend/src/domains/sharing/pages/List.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { AiOutlinePlus } from "react-icons/ai";
+import { QuestionItem } from "../components/QuestionItem";
+import { useQuestion } from "../hooks/useQuestion";
+
+export const QuestionList: React.FC = () => {
+  const { data: questions, isLoading, error } = useQuestion();
+  return (
+    <div className="w-full">
+      <div className="bg-secondary py-3 px-5 flex items-center justify-between">
+        <p>
+          Questions
+          <span className="text-muted-foreground ml-2">
+            {questions?.length}
+          </span>
+        </p>
+
+        <span className="mr-2 hover:cursor-pointer text-muted-foreground">
+          <AiOutlinePlus />
+        </span>
+      </div>
+
+      <div className="w-full flex items-end border-b mt-3 justify-end">
+        <div className="bg-secondary p-2 mr-4 rounded-sm">
+          <Button size="sm" className="bg-white text-muted-foreground">
+            Nouvelles questions
+          </Button>
+          <Button size="sm" className="bg-white text-muted-foreground">
+            Sans réponse
+          </Button>
+        </div>
+      </div>
+
+      <div>
+        {(questions || []).map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))}
+      </div>
+    </div>
+  );
+};
