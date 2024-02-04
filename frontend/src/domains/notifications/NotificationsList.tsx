a1b073547e8d748d5cb34212a9a75c066c1ccd75
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-02-04 13:39:21 +0000

    frontend :: displayed notifications list

--- frontend/src/domains/notifications/NotificationsList.tsx
+++ frontend/src/domains/notifications/NotificationsList.tsx
@@ -1,3 +1,18 @@
+import { useAuth } from "../auth/providers/auth";
+import { NotificationItem } from "./components/NotificationItem";
+import { useNotifications } from "./hook/useNotifications";
+
 export const NotificationsList: React.FC = () => {
-  return <div>Notifications list</div>;
+  const { user } = useAuth();
+  const { data: notifications } = useNotifications(+user!.id);
+  return (
+    <div className="w-full h-full flex">
+      <div className="w-1/3 border-r">
+        {notifications?.map((notification) => (
+          <NotificationItem notification={notification} key={notification.id} />
+        ))}
+      </div>
+      <div className="w-2/3 border"></div>
+    </div>
+  );
 };
