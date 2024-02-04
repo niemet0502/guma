a011a2ff5b9a395afee5c1109b1deac2c745a5cb
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-02-04 13:38:32 +0000

    frontend :: created a custom hook to fetch notifications list

---
+++ frontend/src/domains/notifications/hook/useNotifications.ts
@@ -0,0 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
+import { NotificationApi } from "../type";
+
+const GET_NOTIFICATIONS = gql`
+  query GetNotifications($user_id: Int!) {
+    notifications(user_id: $user_id) {
+      id
+      reminder_id
+      receiver_id
+      created_at
+      read
+      content
+
+      reminder {
+        id
+        title
+      }
+    }
+  }
+`;
+
+export const useNotifications = (userId: number) => {
+  const {
+    data,
+    loading: isLoading,
+    error,
+  } = useQuery<{ notifications: NotificationApi[] }>(GET_NOTIFICATIONS, {
+    variables: { user_id: +userId },
+  });
+  return { data: data?.notifications, isLoading, error };
+};
