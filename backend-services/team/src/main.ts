abf613bae85b349eb4850aa8fdbcfd818149432a
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-03-03 22:30:27 +0000

    backend :: [TeamService] setup logs and http request logging

--- backend-services/team/src/main.ts
+++ backend-services/team/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomLogger } from './logger/custom-logger.service';
 import { otelSDK } from './tracing';
 
 async function bootstrap() {
   await otelSDK.start();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(new CustomLogger());
   app.enableCors();
   await app.listen(3000);
 }
