0b201fa5acf84abc609dd352655536d1ec2de4b2
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-04-22 19:33:09 +0000

    backend :: [ProjectService] clean up

--- backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/dto/CreateLabelInput.java
+++ backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/dto/CreateLabelInput.java
@@ -5,4 +5,27 @@ public class CreateLabelInput {
     private int project_id;
     private int team_id;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getProject_id() {
+        return project_id;
+    }
+
+    public void setProject_id(int project_id) {
+        this.project_id = project_id;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
 }
--- backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/services/LabelsService.java
+++ backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/services/LabelsService.java
@@ -4,8 +4,12 @@ import com.mariusniemet.projectservice.dto.CreateLabelInput;
 import com.mariusniemet.projectservice.dto.UpdateLabelInput;
 import com.mariusniemet.projectservice.models.Label;
 import com.mariusniemet.projectservice.utils.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,13 +20,15 @@ import java.util.Map;
 public class LabelsService {
     private final RestClient client;
     private final  String url = "http://localhost:5002/labels/";
+    private static final Logger logger = LoggerFactory.getLogger(RestClient.class);
 
     public LabelsService(RestClient client) {
         this.client = client;
     }
 
     public Label create(CreateLabelInput createLabelInput){
-        ResponseEntity<Label> response = this.client.post(url, createLabelInput, Label.class);
+
+        ResponseEntity<Label> response = this.client.post("http://localhost:5002/labels", createLabelInput, Label.class);
         return response.getBody();
     }
 
@@ -32,7 +38,6 @@ public class LabelsService {
 
         if(project_id != null){
             queryParams.put("project_id", String.valueOf(project_id));
-
         }
 
         if(team_id != null){
