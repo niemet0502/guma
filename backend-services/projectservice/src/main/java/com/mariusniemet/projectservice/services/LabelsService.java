0d0c499c787f6146ace58665174d83a15bfc13e3
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-04-22 14:37:24 +0000

    backend :: [ProjectService] created the labels service and entity

---
+++ backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/services/LabelsService.java
@@ -0,0 +1,48 @@
+package com.mariusniemet.projectservice.services;
+
+import com.mariusniemet.projectservice.dto.CreateLabelInput;
+import com.mariusniemet.projectservice.dto.UpdateLabelInput;
+import com.mariusniemet.projectservice.models.Label;
+import com.mariusniemet.projectservice.utils.RestClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class LabelsService {
+    private final RestClient client;
+    private final  String url = "http://localhost:5002/labels/";
+
+    public LabelsService(RestClient client) {
+        this.client = client;
+    }
+
+    public Label create(CreateLabelInput createLabelInput){
+        ResponseEntity<Label> response = this.client.post(url, createLabelInput, Label.class);
+        return response.getBody();
+    }
+
+
+    public List<Label> findAll(int project_id, int team_id) {
+        Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("project_id", String.valueOf(project_id));
+        queryParams.put("team_id", String.valueOf(team_id));
+
+        return  this.client.getList(url, queryParams);
+    }
+
+    public Label findOne(int id){
+        ResponseEntity<Label> response = this.client.get(url+ id, Label.class);
+        return response.getBody();
+    }
+
+    public Label update(UpdateLabelInput update){
+        ResponseEntity<Label> response =  this.client.put(url+ update.getId(), update, Label.class);
+        return response.getBody();
+    }
+}
+
