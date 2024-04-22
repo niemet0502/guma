2a19add376e7073cc3f30b7c6554ba9ae866621a
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-04-22 14:33:12 +0000

    backend :: [ProjectService] created the labels resolver

---
+++ backend-services/projectservice/src/main/java/com/mariusniemet/projectservice/resolvers/LabelsResolver.java
@@ -0,0 +1,41 @@
+package com.mariusniemet.projectservice.resolvers;
+
+import com.mariusniemet.projectservice.dto.CreateLabelInput;
+import com.mariusniemet.projectservice.dto.UpdateLabelInput;
+import com.mariusniemet.projectservice.models.Label;
+import com.mariusniemet.projectservice.services.LabelsService;
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
+import com.netflix.graphql.dgs.InputArgument;
+
+import java.util.List;
+
+@DgsComponent
+public class LabelsResolver {
+    private final LabelsService service;
+
+    public LabelsResolver(LabelsService service) {
+        this.service = service;
+    }
+
+    @DgsQuery
+    public List<Label> labels(@InputArgument Integer project_id, @InputArgument Integer team_id){
+        return this.service.findAll(project_id, team_id);
+    }
+
+    @DgsQuery
+    public Label label(@InputArgument Integer id){
+        return this.service.findOne(id);
+    }
+
+    @DgsMutation
+    public Label createLabel(@InputArgument CreateLabelInput createLabelInput){
+        return this.service.create(createLabelInput);
+    }
+
+    @DgsMutation
+    public Label updateLabel(@InputArgument UpdateLabelInput updateLabelInput){
+        return this.service.update(updateLabelInput);
+    }
+}
