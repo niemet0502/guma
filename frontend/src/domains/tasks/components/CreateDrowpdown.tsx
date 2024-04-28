0fe7aefbc908d0be24c2e50e9c1e6abbe73dd5a9
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-04-28 02:08:44 +0000

    frontend :: displayed the card when the task list is empty

--- frontend/src/domains/tasks/components/CreateDrowpdown.tsx
+++ frontend/src/domains/tasks/components/CreateDrowpdown.tsx
@@ -7,22 +7,18 @@ import {
 } from "@/components/ui/dialog";
 import { User } from "@/domains/auth/services/types";
 import { useState } from "react";
-import { AiOutlinePlus } from "react-icons/ai";
 import { CreateTaskForm } from "./CreateForm";
 
 export const CreateTeamDropdown: React.FC<{
   members?: User[];
   teamId?: number;
-}> = ({ members, teamId }) => {
+  children: React.ReactNode;
+}> = ({ members, teamId, children }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <Dialog open={open} onOpenChange={setOpen} modal={false}>
-      <DialogTrigger asChild>
-        <span className="mr-2 hover:cursor-pointer text-muted-foreground">
-          <AiOutlinePlus />
-        </span>
-      </DialogTrigger>
+      <DialogTrigger asChild>{children}</DialogTrigger>
       <DialogContent className="lg:w-[800px] sm:max-w-[625px]">
         <DialogHeader>
           <DialogTitle>Create a new task</DialogTitle>
--- frontend/src/domains/tasks/pages/List.tsx
+++ frontend/src/domains/tasks/pages/List.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useAuth } from "@/domains/auth/providers/auth";
 import { User } from "@/domains/auth/services/types";
@@ -5,6 +6,8 @@ import { useGetTeam } from "@/domains/teams/hooks/useGetTeam";
 import { TeamVisibility } from "@/domains/teams/type";
 import { useGetUsers } from "@/domains/users/hooks/useGetUsers";
 import { useEffect, useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { GoIssueDraft } from "react-icons/go";
 import { useParams } from "react-router-dom";
 import { CreateTeamDropdown } from "../components/CreateDrowpdown";
 import { TaskItem } from "../components/TaskItem";
@@ -31,14 +34,18 @@ export const TaskList: React.FC = () => {
   }, [team]);
 
   return (
-    <div className="w-full">
-      <div className="bg-secondary py-3 px-5 flex items-center justify-between">
+    <div className="w-full h-full">
+      <div className="bg-secondary py-3 px-5 flex items-center justify-between sticky top-0">
         <p>
           Backlog
           <span className="text-muted-foreground ml-2">{tasks?.length}</span>
         </p>
 
-        <CreateTeamDropdown members={members} teamId={team?.id} />
+        <CreateTeamDropdown members={members} teamId={team?.id}>
+          <span className="mr-2 hover:cursor-pointer text-muted-foreground">
+            <AiOutlinePlus />
+          </span>
+        </CreateTeamDropdown>
       </div>
       {isLoading && (
         <div className="flex flex-col gap-2">
@@ -55,6 +62,26 @@ export const TaskList: React.FC = () => {
             .map((task) => (
               <TaskItem task={task} key={task.id} members={members} />
             ))}
+        </div>
+      )}
+
+      {!isLoading && tasks && tasks.length === 0 && (
+        <div className="h-full flex items-center justify-center">
+          <div className="flex flex-col gap-3 border bg-secondary rounded p-8 mb-[150px] w-[400px] shadow-xl">
+            <div className="flex gap-3 items-center">
+              <GoIssueDraft className="text-2xl mt-2" />{" "}
+              <h1 className="text-4xl">Issues</h1>
+            </div>
+            <p>There are no issues for this team yet.</p>
+
+            <p>
+              Once you have created some issues for your team they will show up
+              here.
+            </p>
+            <CreateTeamDropdown members={members} teamId={team?.id}>
+              <Button className="mt-3">Create an issue</Button>
+            </CreateTeamDropdown>
+          </div>
         </div>
       )}
     </div>
