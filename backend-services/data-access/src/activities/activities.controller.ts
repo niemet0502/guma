7695a330da4b30862ccafbef3989efa683f6b0a4
Marius Vincent NIEMET <vincentmarius8@gmail.com>
2024-05-12 00:06:32 +0000

    backend :: [DataAccessService] protected the activities endpoints

--- backend-services/data-access/src/activities/activities.controller.ts
+++ backend-services/data-access/src/activities/activities.controller.ts
@@ -8,6 +8,8 @@ import {
 } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateActivityDto } from '../tasks/dto/create-activity.dto';
+import { User } from '../users/entities/user.entity';
+import { UserDecorator } from '../users/user.decorator';
 import { ActivitiesService } from './activities.service';
 import { Activity } from './entities/activity.entity';
 
@@ -19,9 +21,13 @@ export class ActivitiesController {
   @Post()
   @ApiCreatedResponse({ type: Activity })
   async create(
+    @UserDecorator() user: User,
     @Body() createActivityDto: CreateActivityDto,
   ): Promise<Activity> {
-    return await this.activitiesService.create(createActivityDto);
+    return await this.activitiesService.create({
+      ...createActivityDto,
+      created_by: user.id,
+    });
   }
   @Get()
   @ApiOkResponse({ type: Activity, isArray: true })
--- backend-services/data-access/src/activities/activities.module.ts
+++ backend-services/data-access/src/activities/activities.module.ts
@@ -1,7 +1,14 @@
-import { Module, forwardRef } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  forwardRef,
+} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SprintsModule } from 'src/sprints/sprints.module';
-import { StatusModule } from 'src/status/status.module';
+import { AuthMiddleware } from '../middleware/auth.middleware';
+import { StatusModule } from '../status/status.module';
+import { UsersModule } from '../users/users.module';
 import { ActivitiesController } from './activities.controller';
 import { ActivitiesService } from './activities.service';
 import { Activity } from './entities/activity.entity';
@@ -11,9 +18,14 @@ import { Activity } from './entities/activity.entity';
     TypeOrmModule.forFeature([Activity]),
     StatusModule,
     forwardRef(() => SprintsModule),
+    UsersModule,
   ],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
   exports: [ActivitiesService],
 })
-export class ActivitiesModule {}
+export class ActivitiesModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(ActivitiesController);
+  }
+}
